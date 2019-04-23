078f98d6c9eec606bc34a2a11bbda6fce3547b1b
boreq <boreq@0x46.net>
2019-04-23 21:20:34 +0200

    Replace a custom implementation of utils.Compare with bytes.Compare

--- utils/utils.go
+++ utils/utils.go
@@ -1,9 +1,14 @@
 package utils
 
 import (
+	"bytes"
 	"errors"
 	"os"
 )
+
+// This error is returned by certain functions such as XOR when the length of
+// the provided byte slices is not equal.
+var SliceLengthErr = errors.New("Length of the slices differs")
 
 // EnsureDirExists creates the directory if it doesn't exist. If that is
 // impossible the function returns an error or panics if the second argument is
@@ -29,10 +34,6 @@ func ZerosLen(a []byte) int {
 	return len(a) * 8
 }
 
-// This error is returned by certain functions such as XOR when the length of
-// the provided byte slices is not equal.
-var SliceLengthErr = errors.New("Length of the slices differs")
-
 // XOR runs a[i] ^ b[i] on every element and returns a new slice with a result.
 func XOR(a, b []byte) ([]byte, error) {
 	if len(a) != len(b) {
@@ -57,18 +58,5 @@ func Compare(a, b []byte) (int, error) {
 		return 0, SliceLengthErr
 	}
 
-	for i := 0; i < len(a); i++ {
-		for j := 0; j < 8; j++ {
-			mask := byte(1) << byte(7-j)
-			bitA := a[i] & mask
-			bitB := b[i] & mask
-			if bitA > bitB {
-				return 1, nil
-			}
-			if bitA < bitB {
-				return -1, nil
-			}
-		}
-	}
-	return 0, nil
+	return bytes.Compare(a, b), nil
 }
